7d334b20b41f614e02986c5602c3cb81cf9ef780
alexander.siguenza <alexander.siguenza@gmail.com>
2022-08-23 21:06:13 -0600

    se agrego la clase mayormenor

---
+++ src/sv/edu/ujmd/proyectoconsola/clases/MayorMenor.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.ujmd.proyectoconsola.clases;
+
+/**
+ *
+ * @author Alexander-Siguenza
+ */
+public class MayorMenor {
+    
+    private int num1 = 0;
+    private int num2 = 0;
+    private int num3 = 0;
+    private int mayor= 0;
+    private int menor = 0;
+
+    public MayorMenor() {
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getNum3() {
+        return num3;
+    }
+
+    public void setNum3(int num3) {
+        this.num3 = num3;
+    }
+    
+     public int getMayor() {
+        return mayor;
+    }
+
+    public void setMayor(int mayor) {
+        this.mayor = mayor;
+    }
+
+    public int getMenor() {
+        return menor;
+    }
+
+    public void setMenor(int menor) {
+        this.menor = menor;
+    }    
+    
+    
+    //(return)Funcion o (void)Procedimiento    
+    /*    
+     Procedimiento 1- solicitarNumeros (peticion)  
+     Procedimiento 2- validarEnteros  
+     Procedimiento 3- validarDiferentes   
+     Procedimiento 4- validarMultiplosCinco
+     Procedimiento 5- Calcular numeroMayorMenor
+     Procedimiento 6- validarMayor
+     Procedimiento 7- validarMenor
+     Procedimiento 8- imprimirResultado          
+    */
+
+   
+    
+    
+    
+}
--- src/sv/edu/ujmd/proyectoconsola/clases/Producto.java
+++ src/sv/edu/ujmd/proyectoconsola/clases/Producto.java
@@ -10,5 +10,30 @@ package sv.edu.ujmd.proyectoconsola.clases;
  * @author Alexander-Siguenza
  */
 public class Producto {
+
+    /*
+    PRODUCTO: CANTIDAD: unidades
+        1- producto1 30
+        2- producto2 5
+        3- producto3 60
+        4- producto4 100
+        5- producto5 500
+        6- producto6 20
+        7- producto7 25
+        8- producto8 30
+        9- producto9 180
+        10- producto10 50
+     */
+    /*
+    venta vitaminas -> 10
+    private String producto1 = "Vitaminas 20";
+    private String producto2 = "Aspirinas 300";
+
+    private String[] productos = new String[10];
+    private int[] cantidades   = new int[10];
     
+    [0]Vitaminas,[1]Aspirinas 
+    [0]20       ,[1]300
+    */
+   
 }
